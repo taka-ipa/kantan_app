9b81464ce83c9c10fbaaf51d4b062070099940e6
taka-engy <sake.sloth1110@gmail.com>
2023-11-08 23:15:05 +0900

    ika-ring3 sidebar add

--- ika-ring3.js
+++ ika-ring3.js
@@ -4,7 +4,10 @@ const app1 = new Vue({
     showText: false,
     schedule: {
       1: 'レギュラーマッチ',
-      2: 'バンカラマッチ'
+      2: 'バンカラマッチ',
+      3: 'Xマッチ',
+      4: 'イベントマッチ',
+      5: 'サーモンラン'
     }
   },
   methods: {
@@ -19,8 +22,32 @@ const app2 = new Vue({
   data: {
     showText: false,
     schedule: {
-      1: 'レギュラーマッチ',
-      2: 'バンカラマッチ'
+      1: '最近',
+      2: 'レギュラーマッチ',
+      3: 'バンカラマッチ',
+      4: 'Xマッチ',
+      5: 'イベントマッチ',
+      6: 'プライベートマッチ'
+    }
+  },
+  methods: {
+    toggleText() {
+      this.showText = !this.showText;
+    },
+  },
+});
+
+const app3 = new Vue({
+  el: "#app3",
+  data: {
+    showText: false,
+    schedule: {
+      1: 'ヒストリー',
+      2: 'ブキのきろく',
+      3: 'ステージのきろく',
+      4: 'フェスのきろく',
+      5: 'サーモンランのきろく',
+      6: 'ヒーローモードのきろく'
     }
   },
   methods: {
